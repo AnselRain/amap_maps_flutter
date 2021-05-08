04e45593eafa43665c77b922d822f3f188f109b6
ALAN <857599492@qq.com>
2021-05-08 15:58:44 +0800

    update to 2.0.1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,147 +1,136 @@
+import 'package:amap_flutter_map_example/pages/overlays/marker_add_after_map.dart';
+import 'package:amap_flutter_map_example/pages/overlays/marker_add_with_map.dart';
+import 'package:amap_flutter_map_example/pages/overlays/marker_config.dart';
+import 'package:amap_flutter_map_example/pages/overlays/marker_custom_icon.dart';
+import 'package:amap_flutter_map_example/pages/overlays/place_polygon.dart';
+import 'package:amap_flutter_map_example/pages/overlays/place_polyline.dart';
+import 'package:amap_flutter_map_example/pages/interactive/map_gestures_options.dart';
+import 'package:amap_flutter_map_example/pages/interactive/map_ui_options.dart';
+import 'package:amap_flutter_map_example/pages/interactive/move_camera_demo.dart';
+import 'package:amap_flutter_map_example/pages/interactive/poi_click_demo.dart';
+import 'package:amap_flutter_map_example/pages/interactive/snapshot_demo.dart';
+import 'package:amap_flutter_map_example/pages/map/change_map_type.dart';
+import 'package:amap_flutter_map_example/pages/map/custom_map_style.dart';
+import 'package:amap_flutter_map_example/pages/map/limit_map_bounds.dart';
+import 'package:amap_flutter_map_example/pages/map/map_all_config.dart';
+import 'package:amap_flutter_map_example/pages/map/map_my_location.dart';
+import 'package:amap_flutter_map_example/pages/map/min_max_zoom.dart';
+import 'package:amap_flutter_map_example/pages/map/multi_map.dart';
+import 'package:amap_flutter_map_example/pages/map/show_map_page.dart';
+import 'package:amap_flutter_map_example/pages/overlays/polyline_geodesic.dart';
+import 'package:amap_flutter_map_example/pages/overlays/polyline_texture.dart';
+import 'package:amap_flutter_map_example/widgets/demo_group.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-import 'package:amap_maps_flutter/amap_maps_flutter.dart';
+import 'base_page.dart';
 
-void main() => runApp(MyApp());
+final List<BasePage> _mapDemoPages = <BasePage>[
+  AllMapConfigDemoPage('总体演示', '演示AMapWidget的所有配置项'),
+  ShowMapPage('显示地图', '基本地图显示'),
+  LimitMapBoundsPage('限制地图显示范围', '演示限定手机屏幕显示地图的范围'),
+  MinMaxZoomDemoPage('指定显示级别范围', '演示指定最小最大级别功能'),
+  ChangeMapTypePage('切换地图图层', '演示内置的地图图层'),
+  CustomMapStylePage('自定义地图', '根据自定义的地图样式文件显示地图'),
+  MultiMapDemoPage('地图多实例', '同时显示多个地图'),
+];
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+final List<BasePage> _interactiveDemoPages = <BasePage>[
+  MapUIDemoPage('UI控制', 'ui开关演示'),
+  GesturesDemoPage('手势交互', '手势交互'),
+  PoiClickDemoPage('点击poi功能', '演示点击poi之后的回调和信息透出'),
+  MoveCameraDemoPage('改变地图视角', '演示改变地图的中心点、可视区域、缩放级别等功能'),
+  SnapshotPage('地图截屏', '地图截屏示例'),
+  MyLocationPage('显示我的位置', '在地图上显示我的位置'),
+];
+
+final List<BasePage> _markerPages = <BasePage>[
+  MarkerConfigDemoPage('Marker操作', '演示Marker的相关属性的操作'),
+  MarkerAddWithMapPage("随地图添加", "演示初始化地图时直接添加marker"),
+  MarkerAddAfterMapPage("单独添加", "演示地图初始化之后单独添加marker功能"),
+  MarkerCustomIconPage('自定义图标', '演示marker使用自定义图标功能'),
+];
+
+final List<BasePage> _overlayPages = <BasePage>[
+  PolylineDemoPage('Polyline操作', '演示Polyline的相关属性的操作'),
+  PolylineGeodesicDemoPage('Polyline大地曲线', '演示大地曲线的添加'),
+  PolylineTextureDemoPage('Polyline纹理线', '演示纹理线的添加'),
+  PolygonDemoPage('Polygon操作', '演示Polygon的相关属性的操作'),
+];
+
+final List<Permission> needPermissionList = [
+  Permission.location,
+  Permission.storage,
+  Permission.phone,
+];
+
+class AMapDemo extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter AMap Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter AMap Demo Home Page'),
-    );
+  State<StatefulWidget> createState() {
+    return DemoWidget();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+class DemoWidget extends State<AMapDemo> {
+  @override
+  void initState() {
+    super.initState();
+    _checkPermissions();
+  }
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  void reassemble() {
+    super.reassemble();
+    _checkPermissions();
+  }
 
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
-  AMapController mapController;
-
-  Marker currentMarker;
-
-  bool _isAnimation = true;
+  void _checkPermissions() async {
+    Map<Permission, PermissionStatus> statuses =
+        await needPermissionList.request();
+    statuses.forEach((key, value) {
+      print('$key premissionStatus is $value');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+      appBar: AppBar(title: const Text('高德地图示例')),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                DemoGroupWidget(
+                  groupLabel: '创建地图',
+                  itemPages: _mapDemoPages,
+                ),
+                DemoGroupWidget(
+                  groupLabel: '地图交互',
+                  itemPages: _interactiveDemoPages,
+                ),
+                DemoGroupWidget(
+                  groupLabel: '绘制点标记',
+                  itemPages: _markerPages,
+                ),
+                DemoGroupWidget(
+                  groupLabel: '绘制线和面',
+                  itemPages: _overlayPages,
+                ),
+              ],
+            ),
+          ),
         ),
-        body: Column(children: [
-          Center(
-              child: Container(
-                  padding: EdgeInsets.symmetric(vertical: 30.0),
-                  width: 400.0,
-                  height: 400.0,
-                  child: AMap(
-                    onMapCreated: onMapCreated,
-                  ))),
-          Row(children: <Widget>[
-            FlatButton(
-              child: Text("移动到北京"),
-              onPressed: () {
-                if (mapController != null) {
-                  mapController.changeCamera(CameraPosition(
-                      target: LatLng(39.893927, 116.405972),
-                      zoom: 10
-                  ), _isAnimation);
-                }
-              },),
-            _isAnimationButton(),
-          ],),
-          Row(children: <Widget>[
-            FlatButton(
-              child: Text("添加Marker"),
-              onPressed: () {
-                if (mapController != null) {
-                  MarkerOptions options = MarkerOptions.defaultOptions;
-
-                  mapController.addMarker(options).then(
-                          (marker) {
-                        setState(() {
-                          currentMarker = marker;
-                        }
-                        );
-                      }
-                  );
-                }
-              },),
-            FlatButton(
-              child: Text("修改位置"),
-              onPressed: () {
-                if (mapController != null) {
-                  if (currentMarker != null) {
-                    //fixme 如果默认值不一样如何更新
-                    MarkerOptions markerOptions = MarkerOptions(
-                        position: LatLng(
-                            currentMarker.options.position.latitude,
-                            currentMarker.options.position.longitude + 0.001));
-                    mapController.updateMarker(currentMarker, markerOptions);
-                  } else {
-                    print("currentMarker is null");
-                  }
-                }
-              },),
-          ],),
-          Row(children: <Widget>[
-            FlatButton(
-              child: Text("添加Polyline"),
-              onPressed: () {
-                if (mapController != null) {
-                  MarkerOptions options = MarkerOptions.defaultOptions;
-                  mapController.addMarker(options);
-                }
-              },),
-          ],),
-
-        ]));
-  }
-
-  Widget _isAnimationButton() {
-    return FlatButton(
-      child: Text('${_isAnimation ? '关闭' : '开启'} 动画'),
-      onPressed: () {
-        setState(() {
-          _isAnimation = !_isAnimation;
-        });
-      },
+      ),
     );
   }
+}
 
-
-  void onMapCreated(AMapController controller) {
-    print("onMapCreated");
-    mapController = controller;
-
-    // 注册监听
-    mapController.onMapLoaded.add(onMapLoaded);
-    mapController.onCameraChanged.add(onCameraChanged);
-  }
-
-
-  void onMapLoaded(argument) {
-    print("onMapLoaded");
-  }
-
-  void onCameraChanged(CameraPosition cameraPostion) {
-    print("onCameraChanged " + onCameraChanged.toString());
-  }
-
-
+void main() {
+  // debugProfileBuildsEnabled = true;
+  // debugProfilePaintsEnabled = true;
+  // debugPaintLayerBordersEnabled = true;
+  runApp(MaterialApp(home: AMapDemo()));
 }
--- ios/Classes/AmapMapsFlutterPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface AmapMapsFlutterPlugin : NSObject<FlutterPlugin>
-@end
